cf71434528178b28d40b18532dcf2c4f1ab1480a
cwlizd <cwlizd@gmail.com>
2021-10-29 16:54:48 +0800

    final_products

--- products.py
+++ products.py
@@ -12,3 +12,13 @@ print(products[0][0])
 
 for p in products:
 	print('商品為',p[0],'價格為',p[1])
+
+#字串相加
+#'abc' + '123' = 'abc123'
+#'abc' * 3 = 'abcabcabc'
+# with 一離開那一區間會自動close
+#encoding='utf-8'
+with open('products.csv','w',encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+	    f.write(p[0] + ',' + p[1] + '\n')
