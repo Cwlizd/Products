2408a363d99027b933a5dcadc4b121ece6d12058
Cwlizd <90140055+Cwlizd@users.noreply.github.com>
2021-12-03 10:02:26 +0800

    Add files via upload

--- products.py
+++ products.py
@@ -1,24 +1,40 @@
-# 二維清單概念
-products = []
-total_price = []
-while True:
-    name = input('輸入商品名稱:')
-    if name == 'q':
-    	break
-    price = input('商品價格為:')
-    products.append([name,price])
-print(products)
-print(products[0][0])
+import os
 
-for p in products:
-	print('商品為',p[0],'價格為',p[1])
+#讀取檔案
+def read_file():
+	products = []
+	if os.path.isfile('products.csv'): #檢查檔案是否存在
+		print('find the document!!!')
+		with open('products.csv','r',encoding='utf-8') as f:
+			for line in f:
+				if '商品,價格' in line:
+					continue # 繼續
+				name, price = line.strip().split(',')
+				products.append([name,price])
+			print(products)
+	else:
+		print('找不到檔案')
+		
+#讓使用者輸入
 
-#字串相加
-#'abc' + '123' = 'abc123'
-#'abc' * 3 = 'abcabcabc'
-# with 一離開那一區間會自動close
-#encoding='utf-8'
-with open('products.csv','w',encoding='utf-8') as f:
-	f.write('商品,價格\n')
+def user_input():
+	while True:
+		name = input('請輸入商品名稱:')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格:')
+		price = int(price)
+		products.append([name,price])
+	print(products)
+
+#列印出所有購買紀錄
+def print_products():
 	for p in products:
-	    f.write(p[0] + ',' + p[1] + '\n')
+		print(P[0],'的價格是:',P[1])
+
+#寫入檔案
+def write_file():
+	with open('products.csv','w', encoding='utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
